4c4a6da04fb244ada95c4c40dacf76c6b499e298
Stephen Davis <davi1138@purdue.edu>
2021-07-17 02:13:21 -0400

    Client API v2 (#19)
    * Client API v2
    
    * Updated version to 0.6.0

--- sd_utils/api/client.py
+++ sd_utils/api/client.py
@@ -1,59 +1,45 @@
 from sd_utils.api.error import Error
-from typing import Any, Callable, ClassVar, Dict, Optional, Set
+from typing import Any, ClassVar, Dict, Optional, Set
 from requests.auth import AuthBase
 from sd_utils.api.request import Request
 
 
-def __inject_base_methods(cls: ClassVar[Any]):
-    @classmethod
-    def provide_auth(cls: ClassVar[Any], auth: AuthBase):
-        for _, v in cls.__dict__.items():
-            if isinstance(v, Request):
-                v.auth = auth
+def Client(  # noqa C901
+    url: str, headers: Dict[str, Any] = {}, auth: Optional[AuthBase] = None
+):
+    class client_instance_base(type):
+        base_url: str = url
 
-    setattr(cls, "provide_auth", provide_auth)
+        def __new__(cls, name: str, bases: Any, dct: Any):
+            x = super().__new__(cls, name, bases, dct)
+            errors: Set[Any] = set()
+            for k, v in x.__dict__.items():
+                if isinstance(v, Error):
+                    errors.add(v)
+                    v.set_api_name(k)
+                    setattr(cls, k, v)
 
-    @classmethod
-    def requests(cls: ClassVar[Any]) -> Dict[str, Any]:
-        response: Dict[str, Any] = {}
-        for k, v in cls.__dict__.items():
-            if isinstance(v, Request):
-                response[k] = v
-        return response
+            for k, v in x.__dict__.items():
+                if isinstance(v, Request):
+                    v.configure(url, headers, auth, errors)
+                    setattr(cls, k, v)
 
-    setattr(cls, "requests", requests)
+            return x
 
-    @classmethod
-    def errors(cls: ClassVar[Any]) -> Dict[str, Any]:
-        response: Dict[str, Any] = {}
-        for k, v in cls.__dict__.items():
-            if isinstance(v, Error):
-                response[k] = v
-        return response
+        @classmethod
+        def requests(cls: ClassVar[Any]) -> Dict[str, Any]:
+            response: Dict[str, Any] = {}
+            for k, v in cls.__dict__.items():
+                if isinstance(v, Request):
+                    response[k] = v
+            return response
 
-    setattr(cls, "errors", errors)
+        @classmethod
+        def errors(cls: ClassVar[Any]) -> Dict[str, Any]:
+            response: Dict[str, Any] = {}
+            for k, v in cls.__dict__.items():
+                if isinstance(v, Error):
+                    response[k] = v
+            return response
 
-
-def client_api(
-    base_url: str,
-    headers: Dict[str, Any] = {},
-    auth: Optional[AuthBase] = None,
-) -> Callable[[Any], Any]:
-    def decorator(cls: ClassVar[Any]):
-        errors: Set[Any] = set()
-        for k, v in cls.__dict__.items():
-            if isinstance(v, Error):
-                errors.add(v)
-                v.set_api_name(k)
-                setattr(cls, k, v)
-
-        for k, v in cls.__dict__.items():
-            if isinstance(v, Request):
-                v.configure(base_url, headers, auth, errors)
-                setattr(cls, k, v)
-
-        __inject_base_methods(cls)
-
-        return cls
-
-    return decorator
+    return client_instance_base
